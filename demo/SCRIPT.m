cd6cf8990215f964a2c983ff64cf622807b17945
Ben Vincent <b.t.vincent@dundee.ac.uk>
2015-06-11 20:59:18 +0100

    updated instructions in SCRIPT.m

--- demo/SCRIPT.m
+++ demo/SCRIPT.m
@@ -2,10 +2,21 @@ function SCRIPT
 % code used in the preparation of the paper
 
 %% Preamble
-toolboxPath = '/Users/benvincent/git-local/delayDiscounting/ddToolbox';
+% When you download and use this code, you will have to update these paths
+% to the code you just downloaded. Wherever you put the code, we need to be
+% pointed to */ddToolbox and */demo
+
+% The ddToolbox folder contains the main analysis code. This is essential
+% for using the software on your own dataset.
+toolboxPath = '/Users/benvincent/git-local/delay-discounting-analysis/ddToolbox';
 addpath(genpath(toolboxPath)) 
 
-projectPath = '/Users/benvincent/git-local/delayDiscounting/demo';
+% When you want to run the analysis on your own code, then you must update
+% this path to point at the appropriate folder. This must be arranged in
+% the same way as in the demo folder. In other words, we need participant
+% data files in a folder called data, and a matlab script in the root demo
+% folder (or whatever your project folder is called).
+projectPath = '/Users/benvincent/git-local/delay-discounting-analysis/demo';
 cd(projectPath)
 
 
@@ -29,7 +40,8 @@ fnames={'AC-kirby27-DAYS.txt',...
 'VD-kirby27.txt'};
 % Participant-level data will be aggregated into a larger group-level text
 % file and saved in \data\groupLevelData for inspection. Choose a
-% meaningful filename for this group-level data
+% meaningful filename for this group-level data. This data is not used, it
+% is just provided so you can confirm everything is working properly.
 saveName = 'methodspaper-kirby27.txt';
 
 % create the group-level data object
@@ -39,52 +51,30 @@ kirbyData.loadDataFiles(fnames);
 
 %% Analyse the data with the hierarchical model
 
+% First we create a model object, which we will call hModel. This is an
+% instance of the class 'modelHierarchical'
 hModel = modelHierarchical(toolboxPath);
-% change defaults
+
+% Here we should change default parameters
 hModel.setMCMCtotalSamples(500000); % default is 100,000
 
+% This will initiate MCMC sampling. This can take some time to run,
+% depending on number of samples, chains, computer speed and cores etc.
+% It's probably best to start with a low number of MCMC samples to get a
+% feel for how long the sampling takes.
 hModel.conductInference(kirbyData);
-% plot all the results
+
+% Plot all the results
 hModel.plot(kirbyData)
-% Calculate + plot Bayes Factor for G^m < 0
+
+%% Example research conclusions...
+
+% Calculate Bayes Factor, and plot 95% CI on the posterior
 hModel.HTgroupSlopeLessThanZero(kirbyData)
-
-
 
 % Modal values of m_p for all participants
 % Export these point estimates into a text file and analyse with JASP
 hModel.analyses.univariate.m.mode'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-%% Analyse the same data but with non-hierarchical (independent params) model
-% 
-% sep = modelSeperate(toolboxPath);
-% sep.setMCMCtotalSamples(10000);
-% sep.conductInference(kirbyData);
-% sep.plot(kirbyData)
-% 
-% 
-%% Now do meta-analysis by comparing univariate values from each model
-% clear univariate
-% univariate(1) = hModel.analyses.univariate;
-% univariate(2) = sep.analyses.univariate;
-% % -----------------------------------
-% figGroupedForestPlot(univariate)
-% % EXPORTING -------------------------
-% latex_fig(16, 5, 8)
-% myExport([], [], 'CompareModels-summaryStats')
-% % -----------------------------------
 
 return
