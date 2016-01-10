7d5c91677e84ea6adfd830a475238c12c393d956
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-01-10 13:24:23 +0000

    #23 tidy up loose ends
    - fixed HTgroupSlopeLessThanZero
    - improved variable latex names
    - fixed conditionalDiscountRates()

--- ddToolbox/sampleUtils/HT_BayesFactor.m
+++ ddToolbox/sampleUtils/HT_BayesFactor.m
@@ -2,8 +2,10 @@ function  HT_BayesFactor(obj)
 	warning('IS THERE A MATLAB BAYES FACTOR PACKAGE FOR MCMC SAMPLES?')
 	binsize = 0.05;
 	% extract samples
-	priorSamples = obj.sampler.samples.glMprior(:);
-	posteriorSamples = obj.sampler.samples.glM(:);
+% 	priorSamples = obj.sampler.samples.glMprior(:);
+% 	posteriorSamples = obj.sampler.samples.glM(:);
+	priorSamples = obj.sampler.getSamplesAsMatrix({'m_group_prior'});
+	posteriorSamples = obj.sampler.getSamplesAsMatrix({'m_group'});
 	% in order to evaluate the order-restricted hypothesis m<0, then we need to
 	% remove samples where either prior or posterior contain samples
 	priorSamples = priorSamples(priorSamples<0);
