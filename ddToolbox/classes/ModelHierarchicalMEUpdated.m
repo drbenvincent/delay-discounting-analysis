0bdabd876c63f81ce3132411195f4d65553198b6
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-04-10 14:04:19 +0100

    documentation updates

--- ddToolbox/classes/ModelHierarchicalMEUpdated.m
+++ ddToolbox/classes/ModelHierarchicalMEUpdated.m
@@ -1,4 +1,4 @@
-classdef ModelHierarchicalMEUpdated < ModelHierarchical
+classdef ModelHierarchicalMEUpdated < ModelHierarchicalME
 	%ModelHierarchicalMEUpdated A model to estimate the magnitide effect
 	%   Extends ModelHierarchical but uses new JAGS model with new priors.
 
@@ -9,7 +9,7 @@ classdef ModelHierarchicalMEUpdated < ModelHierarchical
 		% =================================================================
 		function obj = ModelHierarchicalMEUpdated(toolboxPath, sampler, data, saveFolder)
 			% Inherit from ModelHierarchical and override selected methods
-			obj = obj@ModelHierarchical(toolboxPath, sampler, data, saveFolder);
+			obj = obj@ModelHierarchicalME(toolboxPath, sampler, data, saveFolder);
 
 			switch sampler
 				case{'JAGS'}
