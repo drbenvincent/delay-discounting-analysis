3413e3f67b37ea4d4bdb211fdcffcc28ddb28df1
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-06-23 22:30:13 +0100

    small plotting/analysis changes to help with a different research project

--- ddToolbox/model classes/ModelGaussianRandomWalkSimple.m
+++ ddToolbox/model classes/ModelGaussianRandomWalkSimple.m
@@ -69,19 +69,6 @@ classdef ModelGaussianRandomWalkSimple < Model
 
 
 		function plot(obj) % overriding from Model base class
-
-			%% Analyse AUC scores
-			% TODO: Put this is a "generated quantities" function which is
-			% auto-called after JAGS?
-			delays = obj.data.observedData.uniqueDelays;
-			for p=1:obj.data.nParticipants
-				dfSamples = obj.extractDiscountFunctionSamples(p);
-
-				obj.AUC_DATA(p).AUCsamples = calculateAUC(delays,dfSamples, false);
-				%obj.AUC_DATA(p).name = participantName;
-			end
-
-
 			close all
 
 			%% Corner plot of group-level params
@@ -100,6 +87,7 @@ classdef ModelGaussianRandomWalkSimple < Model
 				'saveFolder', obj.saveFolder,...
 				'prefix', 'group')
 
+			
 			%% Plot indifference functions for each participant
 			for p=1:obj.data.nParticipants
 				% Extract info about a person for plotting purposes
@@ -114,110 +102,25 @@ classdef ModelGaussianRandomWalkSimple < Model
 				'saveFolder', obj.saveFolder,...
 				'prefix', personInfo.participantName)
 			end
-
-
-
-
-
-			% CODE BELOW HERE NEEDS TO BE FIXED AND EXTRACTED TO
-			% PROJECT GAIN-LOSS
-
-
-
-% 			% PLOT AUC HERE
-% 			% We have a structure with fields name, Z.
-% 			% The names are suffixed with 'gain' or 'loss', so go through
-% 			% and match up the people.
-% 			all_names = {};
-% 			for p=1:obj.data.nParticipants
-% 				% split name into name + condition
-% 				name = AUC_DATA(p).name;
-% 				parts = strsplit(name,'-');
-% 				name_of_this_person = parts{1};
-% 				condition = parts{2};
-% 				% append name to list if it's the first one, or if it's not
-% 				% already on the list
-% 				if isempty(all_names)
-% 					all_names{1} = name_of_this_person;
-% 				else
-% 					all_names{numel(all_names)+1} = name_of_this_person;
-% 				end
-% 			end
-% 			all_names = unique(all_names);
-% 			% Now loop over people and extract summary info
-% 			n_unique_people = length(all_names);
-% 			for p=1:n_unique_people
-% 				initials = all_names{p};
-%
-% 				% cycle through all people to find their gain
-% 				search_for = strjoin({initials,'gains'},'-');
-% 				for I=1:obj.data.nParticipants
-% 					parts = strsplit(AUC_DATA(I).name,'-');
-% 					first2parts = strjoin({parts{1:2}},'-');
-% 					if strncmp(search_for,first2parts,numel(search_for)-1)
-% 						summary(p).gain_mean = mean(   AUC_DATA(I).Z    );
-% 						UL = prctile(AUC_DATA(I).Z ,[25, 75]);
-% 						summary(p).gain_lower = UL(1);
-% 						summary(p).gain_upper = UL(2);
-% 					end
-% 				end
-%
-% 				% cycle through all people to find their loss
-% 				search_for = strjoin({initials,'loss'},'-');
-% 				for I=1:obj.data.nParticipants
-% 					parts = strsplit(AUC_DATA(I).name,'-');
-% 					first2parts = strjoin({parts{1:2}},'-');
-% 					if strncmp(search_for,first2parts,numel(search_for)-1)
-% 						summary(p).loss_mean = mean(   AUC_DATA(I).Z    );
-% 						UL = prctile(AUC_DATA(I).Z ,[25, 75]);
-% 						summary(p).loss_lower = UL(1);
-% 						summary(p).loss_upper = UL(2);
-% 					end
-% 				end
-% 			end
-%
-% 			summary
-%
-% 			% FINALLY! Plot in losses space
-% 			figure(3), clf
-% 			for n=1:n_unique_people
-% 				hold on
-% 				% point estimate
-% 				%plot( summary(n).loss_mean, summary(n).gain_mean, 'ko')
-% 				% error bar for losses
-% 				plot( [summary(n).loss_lower summary(n).loss_upper], ...
-% 					[summary(n).gain_mean summary(n).gain_mean],'k-')
-% 				% error bar for gains
-% 				plot( [summary(n).loss_mean summary(n).loss_mean], ...
-% 					[summary(n).gain_lower summary(n).gain_upper],'k-')
-% 			end
-% 			xlabel('AUC for losses','Interpreter','latex')
-% 			ylabel('AUC for gains','Interpreter','latex')
-% 			axis([0 3 0 3])
-% 			axis square
-% 			grid on
-%
-% 			hline(1)
-% 			vline(1)
-
-			%set(gca,'XAxisLocation','origin')
-			%set(gca,'YAxisLocation','origin')
-
-% 			delays = obj.data.observedData.dInterp;
-% 			figure(2)
-%
-% 			% extract samples for this participant
-% 			personSamples = squeeze(obj.mcmc.samples.dfInterp(:,:,p,:));
-% 			% collapse over chains
-% 			for d=1:numel(delays)
-% 				dfSamples(:,d) = vec(personSamples(:,:,d));
-% 			end
-% 			subplot(1,obj.data.nParticipants,p)
-% 			ribbon_plot(delays, dfSamples, [0.9 0.9 0.9])
-
+		end
+		
+		
+		function calcAUCscores(obj)
+			%% Analyse AUC scores
+			% TODO: Put this is a "generated quantities" function which is
+			% auto-called after JAGS?
+			delays = obj.data.observedData.uniqueDelays;
+			for p=1:obj.data.nParticipants
+				dfSamples = obj.extractDiscountFunctionSamples(p);
+				
+				obj.AUC_DATA(p).AUCsamples = calculateAUC(delays,dfSamples, false);
+				
+				obj.AUC_DATA(p).name  = obj.data.participantFilenames{p};
+				%obj.AUC_DATA(p).name = participantName;
+			end
 		end
 
-
+		
 		function personStruct = getParticipantData(obj, p)
 			% Create a structure with all the useful info about a person
 			% p = person number
@@ -244,6 +147,9 @@ classdef ModelGaussianRandomWalkSimple < Model
 			end
 		end
 
+		
 	end
+	
 
 end
+
