d44dc7358379c8f1c37beafb230033d1e4458212
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-07-20 18:15:38 +0100

    added welcome message

--- ddToolbox/ddAnalysisSetUp.m
+++ ddToolbox/ddAnalysisSetUp.m
@@ -44,3 +44,15 @@ mcmc.setPlotTheme('fontsize',16, 'linewidth',1)
 % env.dataPath	= p.Results.dataPath;
 
 cd(initialDir)
+
+%% Print welcome
+help_url = 'https://github.com/drbenvincent/delay-discounting-analysis/wiki';
+paper_doi = 'doi:10.3758/s13428-015-0672-2';
+paper_doi_url = 'http://dx.doi.org/10.3758/s13428-015-0672-2';
+
+fprintf('\n\n\n======================================================================================\n')
+fprintf('You are now up and running with:\n')
+fprintf('Hierarchical Bayesian estimation and hypothesis testing for delay discounting tasks\n\n')
+disp(['More information available in the paper at: ' makeHyperlink(paper_doi, makeWeblinkCode(paper_doi_url))] )
+disp(['Help can be found at: ' makeHyperlink(help_url, makeWeblinkCode(help_url))] )
+fprintf('======================================================================================\n\n\n')
