4baeece3c7deff34a2d6de9fb5d386970b8e0ccb
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-07-23 15:49:36 +0100

    #89 works for ModelMixedME [NEW MODEL]

---
+++ ddToolbox/model classes/ModelMixedME.m
@@ -0,0 +1,81 @@
+classdef ModelMixedME < Model
+	%ModelHierarchicalME A model to estimate the magnitide effect
+	%   Detailed explanation goes here
+
+	properties
+	end
+
+
+	methods (Access = public)
+
+		function obj = ModelMixedME(data, varargin)
+			obj = obj@Model(data, varargin{:});
+
+			obj.modelType			= 'mixedME';
+			obj.discountFuncType	= 'me';
+
+			obj.plotFuncs.participantFigFunc = @figParticipantME;
+			obj.plotFuncs.plotGroupLevel = @plotGroupLevelStuff;
+			obj.plotFuncs.clusterPlotFunc = @plotMCclusters;
+
+			%% Create variables
+			% TODO: These lists could be removed with some work
+			obj.varList.participantLevel = {'m', 'c','alpha','epsilon'};
+			%obj.varList.participantLevelPriors = {'m_group_prior', 'c_group_prior','alpha_group_prior','epsilon_group_prior'};
+			%obj.varList.groupLevel = {'m_group', 'c_group','alpha_group','epsilon_group'};
+
+			% These need to be kept for JAGS
+			obj.varList.monitored = {'m', 'c','alpha','epsilon',...
+				'groupMmu', 'groupMsigma', 'groupCmu','groupCsigma','groupW','groupK','groupALPHAmu','groupALPHAsigma',...
+				'groupW_prior','groupK_prior','groupALPHAmu_prior','groupALPHAsigma_prior',... % 'groupMmu_prior', 'groupMsigma_prior', 'groupCmu_prior','groupCsigma_prior',
+				'Rpostpred', 'P'};
+		end
+
+
+		function obj = setInitialParamValues(obj)
+            % Generate initial values of the leaf nodes
+			for chain = 1:obj.sampler.mcmcparams.nchains
+				%obj.initialParams(chain).groupMmu		= normrnd(-0.243,10);
+				%obj.initialParams(chain).groupMsigma	= rand*10;
+				%obj.initialParams(chain).groupCmu		= normrnd(0,30);
+				%obj.initialParams(chain).groupCsigma	= rand*10;
+				obj.initialParams(chain).groupW			= rand;
+				obj.initialParams(chain).groupALPHAmu	= rand*10;
+				obj.initialParams(chain).groupALPHAsigma= rand*10;
+			end
+		end
+
+
+		%% ******** SORT OUT WHERE THESE AND OTHER FUNCTIONS SHOULD BE *************
+		function obj = conditionalDiscountRates(obj, reward, plotFlag)
+			% For group level and all participants, extract and plot P( log(k) | reward)
+			warning('THIS METHOD IS A TOTAL MESS - PLAN THIS AGAIN FROM SCRATCH')
+			obj.conditionalDiscountRates_ParticipantLevel(reward, plotFlag)
+			obj.conditionalDiscountRates_GroupLevel(reward, plotFlag)
+			if plotFlag % FORMATTING OF FIGURE
+				mcmc.removeYaxis()
+				title(sprintf('$P(\\log(k)|$reward=$\\pounds$%d$)$', reward),'Interpreter','latex')
+				xlabel('$\log(k)$','Interpreter','latex')
+				axis square
+				%legend(lh.DisplayName)
+			end
+		end
+
+		function conditionalDiscountRates_GroupLevel(obj, reward, plotFlag)
+			GROUP = obj.data.nParticipants; % last participant is our unobserved
+			params = obj.mcmc.getSamplesFromParticipantAsMatrix(GROUP, {'m','c'});
+			[posteriorMean, lh] = calculateLogK_ConditionOnReward(reward, params, plotFlag);
+			lh.LineWidth = 3;
+			lh.Color= 'k';
+		end
+
+	end
+
+	methods (Access = protected)
+
+		function obj = calcDerivedMeasures(obj)
+		end
+
+	end
+
+end
