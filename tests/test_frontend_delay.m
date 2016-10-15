b71335f2dae254f655ffba474c49679f2b939422
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-10-15 11:09:32 +0100

    #125 Added tests for datasets with front-end delay, DA>0

---
+++ tests/test_frontend_delay.m
@@ -0,0 +1,43 @@
+classdef test_frontend_delay < matlab.unittest.TestCase
+
+	properties
+		data
+		datapath
+		filesToAnalyse
+	end
+	
+	properties (TestParameter)
+		
+	end
+	
+	methods (TestClassSetup)
+		function setup(testCase)
+			% assuming this is running on my maching
+			addpath('~/git-local/delay-discounting-analysis/ddToolbox')
+			testCase.datapath = '~/git-local/delay-discounting-analysis/demo/datasets/test_data';
+			testCase.data = Data(testCase.datapath, 'files', {'fe1.txt', 'fe2.txt', 'fe3.txt', 'fe4.txt'});
+		end
+	end
+	
+
+	
+	methods (Test)
+		
+		function analysis_of_front_end_delay(testCase)
+			% Do an analysis
+			model = ModelHierarchicalLogK(...
+				testCase.data,...
+				'savePath', fullfile(pwd,'output','my_analysis'),...
+				'pointEstimateType', 'median',...
+				'sampler', 'jags',...
+				'shouldPlot', 'yes',...
+				'shouldExportPlots', true,...
+				'mcmcParams', struct('nsamples', 5000,...
+				'nchains', 4,...
+				'nburnin', 500));
+		end
+
+	end
+	
+end
+
