a0050aecb433f332f4b2c27493a8089cf4d5280f
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-10-18 23:27:15 +0100

    #126 added notes + code on repeated measures

--- demo/demo_group_comparison.m
+++ demo/demo_group_comparison.m
@@ -41,7 +41,7 @@ group2 = ModelHierarchicalLogK(...
 	'shouldExportPlots', false,...
 	'mcmcParams', mcmcparams);
 
-%% Compare group level parameter estimates
+%% Compare group level parameter estimates (NOT repeated-measures)
 % ******* NOTE THAT THIS CODE WILL BE IMPROVED AND SIMPLIFIED, BUT THIS IS
 % ENOUGH TO GET THINGS WORKING FOR GROUP-LEVEL COMPARISONS ****************
 
@@ -74,3 +74,51 @@ title('Differences in group level $\log(k)$', 'Interpreter','latex')
 
 
 comparison.group_logk_difference = group_logk_difference;
+
+
+
+
+
+
+
+
+
+warning('Code below this point is under active development')
+
+%% Compare group level parameter estimates (repeated-measures)
+
+% Firstly, we will visualise participant-level changes
+n_participants = 10;
+for p=1:n_participants
+	person_group1_estimate = group1.coda.getSamplesAtIndex(p, {'logk'});
+	person_group2_estimate = group2.coda.getSamplesAtIndex(p, {'logk'});
+	differences(:,p) = person_group1_estimate.logk - person_group2_estimate.logk;
+end
+
+mcmc.UnivariateDistribution(differences,...
+	'XLabel','group 1 - group 2 ($\log(k)$)',...
+	'plotHDI',false)
+
+% If you have a repeated measures design, then we can either:
+%
+% 1) export point estimates of logk by participant and group, then analyse
+% this with a stats package such as SPSS or JASP. This is probably the best
+% thing to do before I have fully worked out the second approach.
+%
+% 2) go down the 'full-Bayesian' route. In this case we would work with
+% full distributions rather than working with point estimates. For example,
+% just as we looked at the distribution of differences between group level
+% estimates, we could do this for each participant, and then look to see if
+% we have meaningful group-level differences in these participant-level
+% differences.
+% While this would be an 'ok' thing to do, it is probably not the best or
+% most kosher way of doing it. If we are doing hierarchical inference, then
+% we may want a new model which estimates participant x condition discount
+% rates, and then hierarchical estimates of participant discount rates etc.
+% This needs some proper thought.
+
+% METHOD 1)
+
+
+% METHOD 2)
+
