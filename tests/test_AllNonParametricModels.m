327c79de0620663d1589791625070aa482786c36
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-10-22 19:50:42 +0100

    removing ModelGRW

--- tests/test_AllNonParametricModels.m
+++ tests/test_AllNonParametricModels.m
@@ -12,7 +12,7 @@ classdef test_AllNonParametricModels < matlab.unittest.TestCase
 	end
 	
 	properties (TestParameter)
-		model = {'ModelNonParametric', 'ModelGRW'}
+		model = {'ModelNonParametric'}
 		pointEstimateType = {'mean','median','mode'}
 		sampler = {'jags', 'stan'} % TODO: ADD STAN
 		chains = {2,3,4}
