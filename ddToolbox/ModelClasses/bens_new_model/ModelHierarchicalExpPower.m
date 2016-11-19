377e2664550fdc6103feb7bb55864fc341358699
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-11-19 21:51:32 +0000

    #147 mixed and hierarchical models (for JAGS) + added to tests

--- ddToolbox/ModelClasses/bens_new_model/ModelHierarchicalExpPower.m
+++ ddToolbox/ModelClasses/bens_new_model/ModelHierarchicalExpPower.m
@@ -4,7 +4,7 @@ classdef ModelHierarchicalExpPower < ExponentialPower
 
 	methods (Access = public)
 
-		function obj = ModelHierarchicalExp1(data, varargin)
+		function obj = ModelHierarchicalExpPower(data, varargin)
 			obj = obj@ExponentialPower(data, varargin{:});
             obj.modelFilename = 'hierarchicalExpPower';
             obj = obj.addUnobservedParticipant('GROUP');
@@ -23,6 +23,8 @@ classdef ModelHierarchicalExpPower < ExponentialPower
 			for chain = 1:nchains
 				initialParams(chain).groupKmu		    = normrnd(0.001,0.1);
 				initialParams(chain).groupKsigma		= rand*5;
+				initialParams(chain).groupTAUmu		    = normrnd(00,0.1); % <---- check
+				initialParams(chain).groupTAUsigma		= rand*5;% <---- check
 				initialParams(chain).groupW				= rand;
 				initialParams(chain).groupALPHAmu		= rand*10;
 				initialParams(chain).groupALPHAsigma	= rand*5;
--- ddToolbox/ModelClasses/bens_new_model/ModelMixedExpPower.m
+++ ddToolbox/ModelClasses/bens_new_model/ModelMixedExpPower.m
@@ -2,7 +2,7 @@ classdef ModelMixedExpPower < ExponentialPower
 	%ModelMixedExp1 
 
 	methods (Access = public)
-		function obj = ModelMixedExp1(data, varargin)
+		function obj = ModelMixedExpPower(data, varargin)
 			obj = obj@ExponentialPower(data, varargin{:});
 			obj.modelFilename = 'mixedExpPower';
             obj = obj.addUnobservedParticipant('GROUP');
@@ -18,6 +18,9 @@ classdef ModelMixedExpPower < ExponentialPower
 			% Generate initial values of the root nodes
 			nExperimentFiles = obj.data.getNExperimentFiles();
 			for chain = 1:nchains
+				%initialParams(chain).k = unifrnd(0, 0.5, [nExperimentFiles,1]);
+                %initialParams(chain).tau = unifrnd(0.01, 2, [nExperimentFiles,1]);
+				
 				initialParams(chain).groupW             = rand;
 				initialParams(chain).groupALPHAmu		= rand*100;
 				initialParams(chain).groupALPHAsigma	= rand*100;
