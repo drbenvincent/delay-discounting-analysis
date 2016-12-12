abada917bb9d2e93d168a2018012c8fb11216007
Ben Vincent <b.t.vincent@dundee.ac.uk>
2016-12-12 09:15:58 +0000

    #153 calling methods from demo_group_comparison

--- ddToolbox/hypothesis-testing/group_comparison.m
+++ ddToolbox/hypothesis-testing/group_comparison.m
@@ -1,4 +1,4 @@
-function group1_minus_group2 = group_comparison(group1, group2, vars, pointEstimateType)
+function group1_minus_group2 = group_comparison(group1, group2, vars, pointEstimateType, method)
 % group1_minus_group2 = group_comparison(group1, group2, {'logk'}, 11)
 
 % input validation
@@ -13,39 +13,40 @@ group2_estimates = group2.getGroupLevelSamples(vars);
 
 % compute the difference
 for var = vars
+	% TODO: make this a stochastic object
 	group1_minus_group2.(var{:}) = group1_estimates.(var{:}) - group2_estimates.(var{:});
 end
 
-%% METHOD 1: Parameter Estimation approach
-% Now you can make decisions about whether there are group level
-% differences based upon whether the 95% credible region overlaps with zero
-% (ie no group difference) or not.
-
-% You could now plot this distribution of differences very simply as:
-% >>  hist(group_logk_difference, 31)
-%
-% but we can use my mcmc-utils-matlab repository code...
-
-for var = vars
-	figure
-	mcmc.UnivariateDistribution(group1_minus_group2.(var{:}),...
-		'XLabel','group 1 - group 2',...
-		'pointEstimateType', pointEstimateType,...
-		'shouldPlotPointEstimate', true)
-	title(['Differences in group level ' var{:}], 'Interpreter','latex')
+switch method
+	case{'parameter estimation'}
+		%% METHOD 1: Parameter Estimation approach
+		
+		% You could now plot this distribution of differences very simply as:
+		% >>  hist(group_logk_difference, 31)
+		%
+		% but we can use my mcmc-utils-matlab repository code...
+		
+		for var = vars
+			figure
+			mcmc.UnivariateDistribution(group1_minus_group2.(var{:}),...
+				'XLabel','group 1 - group 2',...
+				'pointEstimateType', pointEstimateType,...
+				'shouldPlotPointEstimate', true)
+			title(['Differences in group level ' var{:}], 'Interpreter','latex')
+		end
+		
+	case{'bayesian hypothesis test'}
+		%% METHOD 2: Hypothesis testing approach
+		warning('Not yet implemented')
+		
+	case{'point estimates'}
+		%% MERTHOD 3: Estimation with point estimates
+		warning('Not yet implemented')
+		
+		% 1. Extract point estimates.
+		% 2. Run frequentist comparison in Matlab
+		% 3. Export a .csv file of point estimate for analysis in JAGS
+		
 end
-
-
-
-%% METHOD 2: Hypothesis testing approach
-% TODO: BAYESIAN HYPOTHESIS TESTING
-
-
-%% MERTHOD 3: Estimation with point estimates
-% TODO: 
-
-% 1. Extract point estimates.
-% 2. Run frequentist comparison in Matlab
-% 3. Export a .csv file of point estimate for analysis in JAGS
 
 end
