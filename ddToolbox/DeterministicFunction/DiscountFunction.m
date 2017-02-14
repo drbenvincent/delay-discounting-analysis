a16a1ee8a637665c17f15981fb2060e5ffbd114d
Ben Vincent <b.t.vincent@dundee.ac.uk>
2017-02-14 10:39:56 +0000

    DiscountFunction: small refactor + some methods now private

--- ddToolbox/DeterministicFunction/DiscountFunction.m
+++ ddToolbox/DeterministicFunction/DiscountFunction.m
@@ -16,7 +16,7 @@ classdef (Abstract) DiscountFunction < DeterministicFunction
 			timeUnitFunction = str2func(timeUnits);
 			N_SAMPLES_FROM_POSTERIOR = 100;
 			
-			delays = obj.determineDelayValues();
+			delays = obj.getDelayValues();
 			if verLessThan('matlab','9.1') % backward compatability
 				delaysDuration = delays;
 			else
@@ -59,39 +59,36 @@ classdef (Abstract) DiscountFunction < DeterministicFunction
 			
 			drawnow
 		end
-		
-		function delayValues = determineDelayValues(obj)
-			% TODO: remove this stupid special-case handling of group-level
-			% participant with no data
-			try
-				maxDelayRange = max( obj.data.getDelayRange() )*1.2;
-			catch
-				% default (happens when there is no data, ie group level
-				% observer).
-				maxDelayRange = 365;
-			end
-			delayValues = linspace(0, maxDelayRange, 1000);
-		end
-		
-		function nansPresent = anyNaNsPresent(obj)
-			nansPresent = false;
-			for field = fields(obj.theta)'
-				if any(isnan(obj.theta.(field{:}).samples))
-					nansPresent = true;
-					warning('NaN''s detected in theta')
-					break
-				end
-			end
-		end
-		
+        	
 	end
 	
-	methods (Abstract)
-		
-		
-		
-	end
-	
-	
-	
+    
+    methods (Access = private)
+
+        function delayValues = getDelayValues(obj)
+            % TODO: remove this stupid special-case handling of group-level
+            % participant with no data
+            try
+                maxDelayRange = max( obj.data.getDelayRange() )*1.2;
+            catch
+                % default (happens when there is no data, ie group level
+                % observer).
+                maxDelayRange = 365;
+            end
+            delayValues = linspace(0, maxDelayRange, 1000);
+        end
+        
+        function nansPresent = anyNaNsPresent(obj)
+            nansPresent = false;
+            for field = fields(obj.theta)'
+                if any(isnan(obj.theta.(field{:}).samples))
+                    nansPresent = true;
+                    warning('NaN''s detected in theta')
+                    break
+                end
+            end
+        end
+
+    end	
+    
 end
