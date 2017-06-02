7bbb9057e94acff12332c2d1f595a351f72bb942
Ben Vincent <b.t.vincent@dundee.ac.uk>
2017-06-02 15:10:28 +0100

    #192 DRY... removed redundancy with new SubjectiveTimeModel class

--- ddToolbox/models/parametric_models/ebert_and_prelec_2007/EbertPrelec.m
+++ ddToolbox/models/parametric_models/ebert_and_prelec_2007/EbertPrelec.m
@@ -1,11 +1,12 @@
-classdef (Abstract) EbertPrelec < Parametric
+classdef (Abstract) EbertPrelec < SubjectiveTimeModel
 
 	methods (Access = public)
 
 		function obj = EbertPrelec(data, varargin)
-			obj = obj@Parametric(data, varargin{:});
+			obj = obj@SubjectiveTimeModel(data, varargin{:});
             
             obj.dfClass = @DF_EbertPrelec;
+            obj.subjectiveTimeFunctionFH = @SubjectiveTimePowerFunctionEP;
 
 			% Create variables
 			obj.varList.participantLevel = {'k','tau','alpha','epsilon'};
@@ -17,45 +18,6 @@ classdef (Abstract) EbertPrelec < Parametric
             
             obj.plotOptions.dataPlotType = '2D';
 		end
-        
-        % Override this function from superclass
-        function plotExperimentOverviewFigure(obj, ind)
-            %model.plotExperimentOverviewFigure(N) Creates a multi-panel figure
-            %   model.plotExperimentOverviewFigure(N) creates a multi-panel figure
-            %   corresponding to experiment N, where N is an integer.
-            
-            latex_fig(12, 14, 3)
-            h = layout([1 2 3 4 5]);
-            opts.pointEstimateType	= obj.plotOptions.pointEstimateType;
-            opts.timeUnits			= obj.timeUnits;
-            opts.dataPlotType		= obj.plotOptions.dataPlotType;
-            
-            obj.plotPosteriorErrorParams(ind, 'axisHandle', h(1))
-            obj.plotPsychometricFunction(ind, 'axisHandle', h(2))
-            obj.plotPosteriorDiscountFunctionParams(ind, 'axisHandle', h(3))
-            obj.plot_subjective_time_function(ind, 'axisHandle', h(4))
-            obj.plotDiscountFunction(ind, 'axisHandle', h(5))
-        end
-        
-	end
-
-
-    methods (Access = protected)
-
-        function plot_subjective_time_function(obj, ind, varargin)
-            % plot_subjective_time_function
-            % 
-            % Optional arguments as key/value pairs
-            %       'axisHandle' - handle to axes
-            %       'figureHandle' - handle to figure
-            
-            [figureHandle, axisHandle] = parseFigureAndAxisRequested(varargin{:});
-            
-            discountFunctionVariables = obj.getDiscountFunctionVariables();
-            subjectiveTimeFun = SubjectiveTimePowerFunctionEP('samples',...
-                obj.coda.getSamplesAtIndex_asStochastic(ind, discountFunctionVariables));
-            subjectiveTimeFun.plot(obj.plotOptions.pointEstimateType)
-        end
 
     end
     
--- ddToolbox/models/parametric_models/exponential_power_models/ExponentialPower.m
+++ ddToolbox/models/parametric_models/exponential_power_models/ExponentialPower.m
@@ -1,12 +1,13 @@
-classdef (Abstract) ExponentialPower < Parametric
+classdef (Abstract) ExponentialPower < SubjectiveTimeModel
 
 	methods (Access = public)
 
 		function obj = ExponentialPower(data, varargin)
-			obj = obj@Parametric(data, varargin{:});
+			obj = obj@SubjectiveTimeModel(data, varargin{:});
             
             obj.dfClass = @DF_ExponentialPower;
-
+            obj.subjectiveTimeFunctionFH = @SubjectiveTimePowerFunctionEP;
+            
 			% Create variables
 			obj.varList.participantLevel = {'k','tau','alpha','epsilon'};
 			obj.varList.monitored = {'k','tau','alpha','epsilon', 'Rpostpred', 'P', 'VA', 'VB'};
@@ -17,41 +18,6 @@ classdef (Abstract) ExponentialPower < Parametric
             
             obj.plotOptions.dataPlotType = '2D';
 		end
-        
-        % Override this function from superclass
-        function plotExperimentOverviewFigure(obj, ind)
-            latex_fig(12, 14, 3)
-            h = layout([1 2 3 4 5]);
-            opts.pointEstimateType	= obj.plotOptions.pointEstimateType;
-            opts.timeUnits			= obj.timeUnits;
-            opts.dataPlotType		= obj.plotOptions.dataPlotType;
-            
-            obj.plotPosteriorErrorParams(ind, 'axisHandle', h(1))
-            obj.plotPsychometricFunction(ind, 'axisHandle', h(2))
-            obj.plotPosteriorDiscountFunctionParams(ind, 'axisHandle', h(3))
-            obj.plot_subjective_time_function(ind, 'axisHandle', h(4))
-            obj.plotDiscountFunction(ind, 'axisHandle', h(5))
-        end
-        
-	end
-    
-    
-    methods (Access = protected)
-    
-        function plot_subjective_time_function(obj, ind, varargin)
-            % plot_subjective_time_function
-            % 
-            % Optional arguments as key/value pairs
-            %       'axisHandle' - handle to axes
-            %       'figureHandle' - handle to figure
-            
-            [figureHandle, axisHandle] = parseFigureAndAxisRequested(varargin{:});
-            
-            discountFunctionVariables = obj.getDiscountFunctionVariables();
-            subjectiveTimeFun = SubjectiveTimePowerFunction('samples',...
-                obj.coda.getSamplesAtIndex_asStochastic(ind, discountFunctionVariables));
-            subjectiveTimeFun.plot(obj.plotOptions.pointEstimateType)
-        end
     
     end
 
