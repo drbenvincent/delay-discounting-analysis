ece1151e3a63bd804f4b8a50840e2e48784cf67a
Ben Vincent <b.t.vincent@dundee.ac.uk>
2017-08-19 11:23:28 +0100

    adds beta delta discount function (separate model) #199

---
+++ ddToolbox/models/parametric_models/beta_delta_models/BetaDelta.m
@@ -0,0 +1,30 @@
+classdef (Abstract) BetaDelta < Parametric
+	%Hyperbolic1  Hyperbolic1 is a subclass of Model for examining the 1-parameter hyperbolic discounting function.
+
+	methods (Access = public)
+
+		function obj = BetaDelta(data, varargin)
+			obj = obj@Parametric(data, varargin{:});
+
+			obj.dfClass = @DF_BetaDelta;
+
+			% Create variables
+			obj.varList.participantLevel = {'beta','delta','epsilon'};
+			obj.varList.monitored = {'beta','delta', 'alpha','epsilon', 'Rpostpred', 'P', 'VA', 'VB'};
+    		obj.varList.discountFunctionParams(1).name = 'beta';
+			obj.varList.discountFunctionParams(1).label = '$\beta$';
+			obj.varList.discountFunctionParams(2).name = 'delta';
+			obj.varList.discountFunctionParams(2).label = '$delta$';
+
+			obj.plotOptions.dataPlotType = '2D';
+		end
+
+	end
+
+    methods (Hidden = true)
+        function dispModelInfo(obj)
+            display('Discount function: V = beta * delta^delay)')
+        end
+    end
+
+end
---
+++ ddToolbox/models/parametric_models/beta_delta_models/ModelSeparateBetaDelta.m
@@ -0,0 +1,30 @@
+classdef ModelSeparateBetaDelta < BetaDelta
+	%ModelHierarchical A model to estimate the magnitide effect
+	%  NO parameters are estimated hierarchically.
+
+	methods (Access = public, Hidden = true)
+		function obj = ModelSeparateBetaDelta(data, varargin)
+			obj = obj@BetaDelta(data, varargin{:});
+			obj.modelFilename = 'separateBetaDelta';
+
+            % MUST CALL THIS METHOD AT THE END OF ALL MODEL-SUBCLASS CONSTRUCTORS
+            obj = obj.conductInference();
+		end
+    end
+
+    methods (Access = protected)
+
+		function initialParams = initialiseChainValues(obj, nchains)
+            % Generate initial values of the root nodes
+			nExperimentFiles = obj.data.getNExperimentFiles();
+			for chain = 1:nchains
+                initialParams(chain).beta = normrnd(0.84, 2, [nExperimentFiles,1]);
+				initialParams(chain).delta = normrnd(1, 0.5, [nExperimentFiles,1]);
+				initialParams(chain).epsilon = 0.1 + rand([nExperimentFiles,1])/10;
+				initialParams(chain).alpha = abs(normrnd(0.01,10,[nExperimentFiles,1]));
+			end
+		end
+
+	end
+
+end
